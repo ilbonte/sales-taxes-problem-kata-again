8b041c17c54270b68ee8418a7b552ea09e6d3421
tiagomartinho <tiago.martinho@xpeppers.com>
2017-02-01 17:14:23 +0100

    Remove deepEqual

--- test/ProductTest.js
+++ test/ProductTest.js
@@ -1,4 +1,4 @@
-const {equal, deepEqual} = require('assert')
+const {equal} = require('assert')
 
 const Product = require('../src/Product')
 
@@ -27,25 +27,25 @@ test("Product:", function () {
 
   test("tax calculation for a non imported product with rounding up", function(){
     const car = new Product(1, "Astra", 10.22)
-    
+
     equal(1.05, car.salesTaxes)
   })
 
   test("tax calculation for a imported product that is exempt", function(){
     const importedBook = new Product(1, "book", 10.00, true)
-    
+
     equal(0.5, importedBook.salesTaxes)
   })
 
   test("tax calculation for imported and not exempt product", function(){
     const importedCar= new Product(1, "Ferrari", 27.99, true)
-    
+
     equal(4.20, importedCar.salesTaxes)
   })
 
   test("calculate taxes for multiple products", function () {
     const importedCars= new Product(2, "Ferrari", 27.99, true)
-    
+
     equal(8.40, importedCars.salesTaxes)
   })
 
@@ -54,6 +54,5 @@ test("Product:", function () {
     equal(12.60, importedCars.salesTaxes)
     equal(96.57, importedCars.taxedPrice)
   })
-  
-})
 
+})
